51671428dfe4601036dbfcdb840477649e1e40c0
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-08-08 23:36:03 -0500

    completed w/ more numpy practice exercises

--- numpy_exercises.py
+++ numpy_exercises.py
@@ -57,87 +57,91 @@ a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # Use python's built in functionality/operators to determine the following:
 # Exercise 1 - Make a variable called sum_of_a to hold 
 # the sum of all the numbers in above list
-sum_of_a
-
+sum_of_a = np.sum(a)
 
 # Exercise 2 - Make a variable named min_of_a to hold
-#  the minimum of all the numbers in the above list
+# the minimum of all the numbers in the above list
+min_of_a = np.min(a)
 
 # Exercise 3 - Make a variable named max_of_a to hold
-#  the max number of all the numbers in the above list
+# the max number of all the numbers in the above list
+max_of_a = np.max(a)
 
 # Exercise 4 - Make a variable named mean_of_a to hold
-#  the average of all the numbers in the above list
+# the average of all the numbers in the above list
+mean_of_a = np.mean(a)
 
 # Exercise 5 - Make a variable named product_of_a to hold
-#  the product of multiplying all the numbers in the above list together
+# the product of multiplying all the numbers in the above list together
+# https://stackoverflow.com/questions/49863633/numpy-product-vs-numpy-prod-vs-ndarray-prod
+# np.product?
+# np.prod?
+product_of_a = np.prod(a)
 
 # Exercise 6 - Make a variable named squares_of_a. It should hold
-#  each number in a squared like [1, 4, 9, 16, 25...]
+# each number in a squared like [1, 4, 9, 16, 25...]
+squares_of_a = np.square(a)
 
 # Exercise 7 - Make a variable named odds_in_a. It should hold
-#  only the odd numbers
+# only the odd numbers
+# Convert a from list -> numpy.ndarray
+a_array = np.array(a)
+print(type(a_array))
+odds_in_a = a_array[a_array % 2 != 0]
 
 # Exercise 8 - Make a variable named evens_in_a. It should hold
-#  only the evens.
+# only the evens.
+evens_in_a = a_array[a_array % 2 == 0]
 
-## What about life in two dimensions? A list of lists is matrix, a table, a spreadsheet, a chessboard...
-## Setup 2: Consider what it would take to find the sum, min, max, average, sum, product, and list of squares for this list of two lists.
+# What about life in two dimensions? A list of lists is a matrix, a table, a spreadsheet,
+# a chessboard...
+## Setup 2: Consider what it would take to find the sum, min, max, 
+# average, sum, product, and list of squares for this list of two lists.
 b = [
     [3, 4, 5],
     [6, 7, 8]
 ]
 
-# Exercise 1 - refactor the following to use numpy. Use sum_of_b as the variable. **Hint, you'll first need to make sure that the "b" variable is a numpy array**
-sum_of_b = 0
-for row in b:
-    sum_of_b += sum(row)
+# Exercise 1 - refactor the following to use numpy. Use sum_of_b as the variable.
+# **Hint, you'll first need to make sure that the "b" variable is a numpy array**
+b = np.array(b)
+sum_of_b = np.sum(b) 
 
 # Exercise 2 - refactor the following to use numpy. 
-min_of_b = min(b[0]) if min(b[0]) <= min(b[1]) else min(b[1])  
+min_of_b = np.min(b)
 
 # Exercise 3 - refactor the following maximum calculation to find the answer with numpy.
-max_of_b = max(b[0]) if max(b[0]) >= max(b[1]) else max(b[1])
-
+max_of_b = np.max(b)
 
 # Exercise 4 - refactor the following using numpy to find the mean of b
-mean_of_b = (sum(b[0]) + sum(b[1])) / (len(b[0]) + len(b[1]))
+mean_of_b = np.mean(b)
 
-# Exercise 5 - refactor the following to use numpy for calculating the product of all numbers multiplied together.
-product_of_b = 1
-for row in b:
-    for number in row:
-        product_of_b *= number
+# Exercise 5 - refactor the following to use numpy for calculating
+# the product of all numbers multiplied together.
+product_of_b = np.prod(b)
 
 # Exercise 6 - refactor the following to use numpy to find the list of squares 
-squares_of_b = []
-for row in b:
-    for number in row:
-        squares_of_b.append(number**2)
-
+squares_of_b = np.square(b)
 
 # Exercise 7 - refactor using numpy to determine the odds_in_b
-odds_in_b = []
-for row in b:
-    for number in row:
-        if(number % 2 != 0):
-            odds_in_b.append(number)
-
+odds_in_b = b[b % 2 != 0]
 
 # Exercise 8 - refactor the following to use numpy to filter only the even numbers
-evens_in_b = []
-for row in b:
-    for number in row:
-        if(number % 2 == 0):
-            evens_in_b.append(number)
+evens_in_b = b[b % 2 == 0]
 
 # Exercise 9 - print out the shape of the array b.
+print(b.shape)
 
 # Exercise 10 - transpose the array b.
+b_transpose = b.T
 
 # Exercise 11 - reshape the array b to be a single list of 6 numbers. (1 x 6)
+b_reshape_list = b.reshape(1, 6)
 
-# Exercise 12 - reshape the array b to be a list of 6 lists, each containing only 1 number (6 x 1)
+# Exercise 12 - reshape the array b to be a list of 6 lists, 
+# each containing only 1 number (6 x 1)
+b_reshape_list_of_lists = b.reshape(6, 1)
+
 
 ## Setup 3
 c = [
@@ -146,22 +150,40 @@ c = [
     [7, 8, 9]
 ]
 
-# HINT, you'll first need to make sure that the "c" variable is a numpy array prior to using numpy array methods.
+# HINT, you'll first need to make sure that the "c" variable 
+# is a numpy array prior to using numpy array methods.
+c = np.array(c)
 # Exercise 1 - Find the min, max, sum, and product of c.
+min_of_c = np.min(c)
+max_of_c = np.max(c)
+sum_of_c = np.sum(c)
+product_of_c = np.prod(c)
 
 # Exercise 2 - Determine the standard deviation of c.
+stddev_of_c = np.std(c)
 
 # Exercise 3 - Determine the variance of c.
+variance_of_c = np.var(c)
 
 # Exercise 4 - Print out the shape of the array c
+print(c.shape)
 
 # Exercise 5 - Transpose c and print out transposed result.
+c_transpose = c.T
+print(c_transpose)
 
 # Exercise 6 - Get the dot product of the array c with c. 
+c_dot_product = c.dot(c)
 
-# Exercise 7 - Write the code necessary to sum up the result of c times c transposed. Answer should be 261
+# Exercise 7 - Write the code necessary to sum up the result of c times c transposed.
+# Answer should be 261
+sum_c_times_c_transposed = np.sum(c * c_transpose)
+print(sum_c_times_c_transposed)
 
-# Exercise 8 - Write the code necessary to determine the product of c times c transposed. Answer should be 131681894400.
+# Exercise 8 - Write the code necessary to determine the product of c times c transposed.
+# Answer should be 131681894400.
+prod_c_times_c_transposed = np.prod(c * c_transpose)
+print(prod_c_times_c_transposed)
 
 
 ## Setup 4
@@ -170,25 +192,35 @@ d = [
     [45, -90, -30, 270, 90, 0],
     [60, 45, -45, 90, -45, 180]
 ]
+d = np.array(d)
 
 # Exercise 1 - Find the sine of all the numbers in d
+d_sine = np.sin(d)
 
 # Exercise 2 - Find the cosine of all the numbers in d
+d_cosine = np.cos(d)
 
 # Exercise 3 - Find the tangent of all the numbers in d
+d_tangent = np.tan(d)
 
 # Exercise 4 - Find all the negative numbers in d
+negative_nums_in_d = d[d < 0]
 
 # Exercise 5 - Find all the positive numbers in d
+positive_nums_in_d = d[d > 0]
 
 # Exercise 6 - Return an array of only the unique numbers in d.
+d_unique = np.unique(d)
 
 # Exercise 7 - Determine how many unique numbers there are in d.
+d_unique_count = len(d_unique)
 
 # Exercise 8 - Print out the shape of d.
+print(d.shape)
 
 # Exercise 9 - Transpose and then print out the shape of d.
+d_transpose = np.transpose(d)
 
 # Exercise 10 - Reshape d into an array of 9 x 2
-
-
+d_reshape = d.reshape(9, 2)
+print(d_reshape)
