b1c024dc41b9371f8038d21976396d412d27ac55
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-08-10 14:47:05 -0500

    recoded examples to reflect lecture best practices

--- numpy_exercises.py
+++ numpy_exercises.py
@@ -10,32 +10,33 @@ a = np.array([4, 10, 12, 23, -2, -1, 0, 0, 0, -6, 3, -7])
 # I found `np.count_zero` while looking for examples.
 # https://github.com/numpy/numpy/issues/8913
 # negative_numbers = np.count_nonzero(a < 0)
-negative_numbers = np.sum(a < 0)
+negative_numbers = (a < 0).sum()
 print(f"Negative numbers: {negative_numbers}")
 
 
 # How many positive numbers are there?
 # positive_numbers = np.count_nonzero(a > 0)
-positive_numbers = np.sum(a > 0)
+positive_numbers = (a > 0).sum()
 print(f"Positive numbers: {positive_numbers}")
 
 # How many even positive numbers are there?
-positive_evens = np.sum(a[a > 0] % 2 == 0)
+# positive_evens = np.sum(a[a > 0] % 2 == 0)
+positive_evens = ((a > 0) & (a % 2 == 0)).sum()
 print(f"Positive even numbers: {positive_evens}")
 
 # If you were to add 3 to each data point, how many positive numbers would there be?
-plus_3_postive_numbers = np.sum(a + 3 > 0)
+plus_3_postive_numbers = (a + 3 > 0).sum()
 print(f"Postive numbers after a + 3: {plus_3_postive_numbers}")
 
 # If you squared each number, what would the new mean and standard deviation be?
-a_mean = np.mean(a)
-a_stddev = np.std(a)
+a_mean = a.mean(dtype=np.float64)
+a_stddev = a.std(dtype=np.float64)
 print("\na's Mean and Standard Deviation BEFORE -> a ** 2")
 print("-" * 40)
 print("Mean --- {}\nStandard Deviation --- {}".format(a_mean, a_stddev))
 
-a_mean_squared = np.mean(a ** 2, dtype=np.float64)
-a_stddev_squared = np.std(a ** 2, dtype=np.float64)
+a_mean_squared = (a ** 2).mean(dtype=np.float64)
+a_stddev_squared = (a ** 2).std(dtype=np.float64)
 print("\na's Mean and Standard Deviation AFTER -> a ** 2")
 print("-" * 40)
 print("Mean --- {}\nStandard Deviation --- {}\n".format(a_mean_squared, a_stddev_squared))
@@ -57,41 +58,43 @@ a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # Use python's built in functionality/operators to determine the following:
 # Exercise 1 - Make a variable called sum_of_a to hold 
 # the sum of all the numbers in above list
-sum_of_a = np.sum(a)
+sum_of_a = sum(a)
 
 # Exercise 2 - Make a variable named min_of_a to hold
 # the minimum of all the numbers in the above list
-min_of_a = np.min(a)
+min_of_a = min(a)
 
 # Exercise 3 - Make a variable named max_of_a to hold
 # the max number of all the numbers in the above list
-max_of_a = np.max(a)
+max_of_a = max(a)
 
 # Exercise 4 - Make a variable named mean_of_a to hold
 # the average of all the numbers in the above list
-mean_of_a = np.mean(a)
+mean_of_a = sum_of_a / len(a)
 
 # Exercise 5 - Make a variable named product_of_a to hold
 # the product of multiplying all the numbers in the above list together
 # https://stackoverflow.com/questions/49863633/numpy-product-vs-numpy-prod-vs-ndarray-prod
 # np.product?
 # np.prod?
-product_of_a = np.prod(a)
+product_of_a = 1
+
+for num in a:
+    product_of_a *= num
 
 # Exercise 6 - Make a variable named squares_of_a. It should hold
 # each number in a squared like [1, 4, 9, 16, 25...]
-squares_of_a = np.square(a)
+squares_of_a = [num ** 2 for num in a]
 
 # Exercise 7 - Make a variable named odds_in_a. It should hold
 # only the odd numbers
-# Convert a from list -> numpy.ndarray
-a_array = np.array(a)
-print(type(a_array))
-odds_in_a = a_array[a_array % 2 != 0]
+
+odds_in_a = [num for num in a if(num % 2 != 0)]
+print(odds_in_a)
 
 # Exercise 8 - Make a variable named evens_in_a. It should hold
 # only the evens.
-evens_in_a = a_array[a_array % 2 == 0]
+evens_in_a = [num for num in a if num % 2 == 0]
 
 # What about life in two dimensions? A list of lists is a matrix, a table, a spreadsheet,
 # a chessboard...
@@ -105,20 +108,20 @@ b = [
 # Exercise 1 - refactor the following to use numpy. Use sum_of_b as the variable.
 # **Hint, you'll first need to make sure that the "b" variable is a numpy array**
 b = np.array(b)
-sum_of_b = np.sum(b) 
+sum_of_b = b.sum()
 
 # Exercise 2 - refactor the following to use numpy. 
-min_of_b = np.min(b)
+min_of_b = b.min()
 
 # Exercise 3 - refactor the following maximum calculation to find the answer with numpy.
-max_of_b = np.max(b)
+max_of_b = b.max()
 
 # Exercise 4 - refactor the following using numpy to find the mean of b
-mean_of_b = np.mean(b)
+mean_of_b = b.mean()
 
 # Exercise 5 - refactor the following to use numpy for calculating
 # the product of all numbers multiplied together.
-product_of_b = np.prod(b)
+product_of_b = b.prod()
 
 # Exercise 6 - refactor the following to use numpy to find the list of squares 
 squares_of_b = np.square(b)
@@ -154,17 +157,16 @@ c = [
 # is a numpy array prior to using numpy array methods.
 c = np.array(c)
 # Exercise 1 - Find the min, max, sum, and product of c.
-min_of_c = np.min(c)
-max_of_c = np.max(c)
-sum_of_c = np.sum(c)
-product_of_c = np.prod(c)
+min_of_c = c.min()
+max_of_c = c.max()
+sum_of_c = c.sum()
+product_of_c = c.prod()
 
 # Exercise 2 - Determine the standard deviation of c.
-stddev_of_c = np.std(c)
+stddev_of_c = c.std()
 
 # Exercise 3 - Determine the variance of c.
-variance_of_c = np.var(c)
-
+variance_of_c = c.var()
 # Exercise 4 - Print out the shape of the array c
 print(c.shape)
 
@@ -177,12 +179,12 @@ c_dot_product = c.dot(c)
 
 # Exercise 7 - Write the code necessary to sum up the result of c times c transposed.
 # Answer should be 261
-sum_c_times_c_transposed = np.sum(c * c_transpose)
+sum_c_times_c_transposed = (c * c_transpose).sum()
 print(sum_c_times_c_transposed)
 
 # Exercise 8 - Write the code necessary to determine the product of c times c transposed.
 # Answer should be 131681894400.
-prod_c_times_c_transposed = np.prod(c * c_transpose)
+prod_c_times_c_transposed = (c * c_transpose).prod()
 print(prod_c_times_c_transposed)
 
 
@@ -213,13 +215,13 @@ positive_nums_in_d = d[d > 0]
 d_unique = np.unique(d)
 
 # Exercise 7 - Determine how many unique numbers there are in d.
-d_unique_count = len(d_unique)
-
+d_unique_count = len(np.unique(d))
+print(d_unique_count)
 # Exercise 8 - Print out the shape of d.
 print(d.shape)
 
 # Exercise 9 - Transpose and then print out the shape of d.
-d_transpose = np.transpose(d)
+d_transpose = d.transpose()
 
 # Exercise 10 - Reshape d into an array of 9 x 2
 d_reshape = d.reshape(9, 2)
